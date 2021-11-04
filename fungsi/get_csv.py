43252199554042cc78a2b59aa2a4ea1707f5dbf8
williamnixon20 <75229742+williamnixon20@users.noreply.github.com>
2021-11-04 11:52:12 +0700

    minor bugfix

--- fungsi/get_csv.py
+++ fungsi/get_csv.py
@@ -32,21 +32,29 @@ def save_csv():
                     os.makedirs(f"fungsi/currency_dfs/{curr[0]}")
 
                 for row in table.findAll("tr")[1:]:
+                    currency = row.findAll("td")[0].text
+                    for abbv in curr_list:
+                        if currency == abbv[1]:
+                            currency = abbv[0]
                     exchange = row.findAll("td")[1].find('a').contents[0]
-                    currs.append([day.strftime('%Y-%m-%d'), exchange])
+                    currs.append(
+                        [day.strftime('%Y-%m-%d'), exchange, currency])
 
                 for row in currs:
-                    if not os.path.exists(f"fungsi/currency_dfs/{curr[0]}/fx_daily_{curr[0]}_{row[1]}.csv"):
-                        with open(f"fungsi/currency_dfs/{curr[0]}/fx_daily_{curr[0]}_{row[1]}.csv", "w", encoding="UTF8", newline='') as f:
+                    if not os.path.exists(f"fungsi/currency_dfs/{curr[0]}/fx_daily_{curr[0]}_{row[2]}.csv"):
+                        with open(f"fungsi/currency_dfs/{curr[0]}/fx_daily_{curr[0]}_{row[2]}.csv", "w", encoding="UTF8", newline='') as f:
                             writer = csv.writer(f)
-                            writer.writerow(["date", "currency", "rate"])
-                            writer.writerow(row)
+                            writer.writerow(["date", "rate"])
+                            writer.writerow([row[0], row[1]])
                         print(f"W: {day} {curr[0]} {row[1]}")
                     else:
-                        with open(f"fungsi/currency_dfs/{curr[0]}/fx_daily_{curr[0]}_{row[1]}.csv", "a", encoding="UTF8", newline='') as f:
+                        with open(f"fungsi/currency_dfs/{curr[0]}/fx_daily_{curr[0]}_{row[2]}.csv", "a", encoding="UTF8", newline='') as f:
                             writer = csv.writer(f)
-                            writer.writerow(row)
+                            writer.writerow([row[0], row[1]])
                             f.close()
                         print(f"A: {day} {curr[0]} {row[1]}")
     else:
         print("Today's rate has been extracted")
+
+
+save_csv()
